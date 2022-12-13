6474600874fe13cbdf3ebe7ca838066c619d89cb
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2022-12-13 18:43:18 +0600

    All routes, containers and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useEffect, useState} from 'react';
+import NavBar from "./components/NavBar/NavBar";
+import {Route, Routes, useLocation} from "react-router-dom";
+import Home from "./containers/Home/Home";
+import axiosApi from "./axiosApi";
+import {MealApi, MealType} from "./types";
+import NewMeal from "./containers/NewMeal/NewMeal";
+import EditMeal from "./containers/EditMeal/EditMeal";
 
 function App() {
+  const location = useLocation();
+  const [meals, setMeals] = useState<MealType[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const getMeals = useCallback(async () => {
+    try {
+      setLoading(true);
+      const mealsResponse = await axiosApi.get<MealApi | null>('meals.json');
+      const meals = mealsResponse.data;
+
+      let newMeals: MealType[] = [];
+
+      if (meals) {
+        newMeals = Object.keys(meals).map(id => {
+          const meal = meals[id];
+          return {
+            ...meal,
+            id
+          }
+        });
+
+      }
+      setMeals(newMeals);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    void getMeals();
+  }, [location, getMeals]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <NavBar/>
       </header>
-    </div>
+      <main className="container-fluid">
+        <Routes>
+          <Route path="/" element={<Home meals={meals} mealIsLoading={loading} getMeals={getMeals}/>}/>
+          <Route path="/new-meal" element={<NewMeal/>}/>
+          <Route path="/edit-meal/:id" element={<EditMeal/>}/>
+        </Routes>
+      </main>
+    </>
   );
 }
 
